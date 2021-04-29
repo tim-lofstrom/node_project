ee1b5994a45885f87c2ea9103fd27fbfd17bec67
Tim Löfström <tim.lofstrom.danielsson@lm.se>
2021-04-29 11:38:26 +0200

    update

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
     res.send("asd");
 })
 
-app.listen(8888, '127.0.0.1', function() {
+app.listen(3000, '127.0.0.1', function() {
     console.log("Server now listening on 3000");
 });
